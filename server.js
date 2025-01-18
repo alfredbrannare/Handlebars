68f7cc365e81710cf1b811646d53510f1fac989d
Alfred <alfred-brannarer@hotmail.com>
2025-01-18 12:11:06 +0100

    Added /movies and /movie/:movieId endpoints

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 import express from 'express';
 import ejs from 'ejs';
 import navLinks from './src/js/server/navLinks.js';
+import { fetchAllMovies } from './src/js/server/fetchMovies.js';
+import { fetchMovie } from './src/js/server/fetchMovies.js';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const linkData = navLinks();
-  res.render('pages/index', { ...linkData });
+  const moviesData = await fetchAllMovies();
+  res.render('pages/index', { moviesData, ...linkData });
 });
 
-app.get('/movies', (req, res) => {
+app.get('/movies', async (req, res) => {
   const linkData = navLinks();
-  res.render('pages/movies', { ...linkData });
+  const moviesData = await fetchAllMovies();
+  res.render('pages/allMovies', { moviesData, ...linkData });
+});
+
+app.get('/movie/:movieId', async (req, res) => {
+  const linkData = navLinks();
+  const movieData = await fetchMovie(req.params.movieId);
+  res.render('pages/movie', { movieData, ...linkData });
 });
 
 app.use('/static', express.static('./static'));
