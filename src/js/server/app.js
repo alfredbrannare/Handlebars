f3d1c4af7df578161036e2ec154c2bb424a9d64c
Alfred <alfred-brannarer@hotmail.com>
2025-01-21 11:23:21 +0100

    Switch from EJS to Handlebars

--- src/js/server/app.js
+++ src/js/server/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import ejs from 'ejs';
+import { engine } from 'express-handlebars';
 import { marked } from 'marked';
 
 function startApp(api, nav) {
   const app = express();
 
   app.locals.formatMarkdown = (text) => marked(text);
-  app.set('view engine', 'ejs');
+  app.engine('handlebars', engine());
+  app.set('view engine', 'handlebars');
+  app.set('views', './templates');
 
   app.get('/', async (req, res) => {
     try {
@@ -21,7 +23,7 @@ function startApp(api, nav) {
         return res.status(500).send('Movies or Links data is missing or malformed');
       }
 
-      res.render('pages/index', { moviesData, ...linkData });
+      res.render('index', { moviesData, ...linkData });
     } catch (error) {
       console.error('Error fetching data:', error);
       res.status(500).send('Error occurred while fetching data from API or navLinks');
@@ -31,20 +33,20 @@ function startApp(api, nav) {
   app.get('/movies', async (req, res) => {
     const linkData = await nav.navLinks();
     const moviesData = await api.fetchAllMovies();
-    res.render('pages/allMovies', { moviesData, ...linkData });
+    res.render('allMovies', { moviesData, ...linkData });
   });
 
   app.get('/movie/:movieId', async (req, res) => {
     const linkData = await nav.navLinks();
     const movieData = await api.fetchMovie(req.params.movieId);
-    res.render('pages/movie', { movieData, ...linkData });
+    res.render('movie', { movieData, ...linkData });
   });
 
   app.use('/static', express.static('./static'));
 
   app.use((req, res) => {
     const linkData = nav.navLinks();
-    res.status(404).render('pages/error', { message: 'Page not found', ...linkData });
+    res.status(404).render('error', { message: 'Page not found', ...linkData });
   });
 
   return app;
